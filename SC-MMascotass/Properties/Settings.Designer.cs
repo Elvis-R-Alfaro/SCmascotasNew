6e7563ce29242c67799ef65d2f0bde6d2b1a0268
Elvis-R-Alfaro <82235597+Elvis-R-Alfaro@users.noreply.github.com>
2021-08-01 14:39:13 -0600

    Nueva Conexion BD

--- SC-MMascotass/Properties/Settings.Designer.cs
+++ SC-MMascotass/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace SC_MMascotass.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DAST\\MSSQLSERVER2019;Initial Catalog=scmascotas2;Integrated Security=" +
-            "True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.193.10;Initial Catalog=scmascotas2;User ID=remoto;Password=12" +
+            "3456")]
         public string scmascotas2ConnectionString {
             get {
                 return ((string)(this["scmascotas2ConnectionString"]));
