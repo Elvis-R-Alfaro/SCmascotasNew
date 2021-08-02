9b8f46e72d4b2712d72743df439bbe223d1e4f8c
Elvis-R-Alfaro <82235597+Elvis-R-Alfaro@users.noreply.github.com>
2021-08-02 13:56:02 -0600

    Cargar los datos al editar en los combobox

--- SC-MMascotass/Pages/Formularios/FormRazas.xaml.cs
+++ SC-MMascotass/Pages/Formularios/FormRazas.xaml.cs
@@ -28,12 +28,20 @@ namespace SC_MMascotass.Pages.Formularios
     {
         private SqlConnection sqlConnection = database.Conexion.ObtenerConexion();
         private Mascota mascota = new Mascota();
+        private List<Mascota> mascotas;
         public FormRazas(bool visible)
         {
             InitializeComponent();
             CargarEspeciesCombo();
+            ObtenerRazas();
         }
 
+        private void ObtenerRazas()
+        {
+            mascotas = Constructores.Procedimientos.MostrarRaza();
+            dgClientes.SelectedValuePath = "IdRaza";
+            dgClientes.ItemsSource = mascotas;
+        }
         private void CargarEspeciesCombo()
         {
             try
@@ -110,36 +118,21 @@ namespace SC_MMascotass.Pages.Formularios
             {
                 try
                 {
-                    //Query de insertar
-                    string query = @"INSERT INTO Veterinaria.Raza
-                                   (IdEspecie
-                                   ,NombreRaza
-                                   ,Altura
-                                   ,RangoPeso
-                                   ,EsperanzaVida
-                                   ,ActividadFisica
-                                   ,TipoDePelo)
-                             VALUES
-                                   ()";
+                    //Obtener los valores para la mascota
+                    mascota.IdEspecie = Convert.ToInt32(cmbesoecie.SelectedValuePath);
+                    mascota.NombreRaza = txtNombreRaza.Text;
+                    mascota.Altura = cmbaltura.SelectionBoxItem.ToString();
+                    mascota.RangoPeso = cmbPesoIdeal.SelectionBoxItem.ToString();
+                    mascota.EsperanzaVida = cmbEsperanzaVida.SelectionBoxItem.ToString();
+                    mascota.ActividadFisica = cmbActividadFisica.SelectionBoxItem.ToString();
+                    mascota.TipoDePelo = txtTipoPelo.Text;
 
-                    //Establecer la conexion
-                    sqlConnection.Open();
+                    //Ejecutamos
+                    Constructores.Procedimientos.CrearRaza(mascota);
 
-
-
-                    //Crear el comando SQL
-                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-
-                    //Establecer los valores de los paramawtros
-                    //sqlCommand.Parameters.AddWithValue("@IdCliente", mascota.IdCliente);
-                    //sqlCommand.Parameters.AddWithValue("@AliasMascota", mascota.NombreMascota);
-                    //sqlCommand.Parameters.AddWithValue("@Raza", mascota.NombreRaza);
-                    //sqlCommand.Parameters.AddWithValue("@Fecha", mascota.Fecha);
-
-                    //ejecutar el comando insertado
-                    sqlCommand.ExecuteNonQuery();
-
-                    sqlConnection.Close();
+                    //Mensaje de inserccion exito
+                    MessageBox.Show("Datos Insertados Correctamente", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ObtenerRazas();
                 }
                 catch (Exception ex)
                 {
@@ -148,15 +141,21 @@ namespace SC_MMascotass.Pages.Formularios
                 }
                 finally
                 {
-                    this.Close();
+                    Limpiar();
 
                 }
             }
         }
 
+        private void Limpiar()
+        {
+            txtNombreRaza.Text = "";
+            txtTipoPelo.Text = "";
+        }
+
         private void btnRestablecer_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void btnAceptar_Click(object sender, RoutedEventArgs e)
@@ -173,6 +172,69 @@ namespace SC_MMascotass.Pages.Formularios
         {
             Formularios.FormEspecies especie = new Formularios.FormEspecies(false);
             especie.Show();
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgClientes.SelectedValue == null)
+                MessageBox.Show("Por favor selecciona una especie de la lista");
+            else
+            {
+                //spButton1.Visibility = Visibility.Hidden;
+                //spButton2.Visibility = Visibility.Visible;
+                try
+                {
+                    //Query busqueda
+                    string query = @"SELECT Veterinaria.Raza.*, Veterinaria.Especie.Descripcion
+                        FROM     Veterinaria.Especie INNER JOIN
+                  Veterinaria.Raza ON Veterinaria.Especie.IdEspecie = Veterinaria.Raza.IdEspecie
+				  WHERE  (Veterinaria.Raza.IdRaza = @IdRaza)";
+
+                    //Establecer la coneccion
+                    sqlConnection.Open();
+
+                    //Crear el comando SQL
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                    //Establecer el valor del parametro
+                    sqlCommand.Parameters.AddWithValue("@IdRaza", Convert.ToInt32(dgClientes.SelectedValue));
+
+                    using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            txtNombreRaza.Text = rdr["NombreRaza"].ToString();
+                            cmbaltura.Text = rdr["Altura"].ToString();
+                            cmbActividadFisica.Text = rdr["ActividadFisica"].ToString();
+                            cmbPesoIdeal.Text = rdr["RangoPeso"].ToString();
+                            cmbesoecie.Text = rdr["Descripcion"].ToString();
+                            cmbEsperanzaVida.Text = rdr["EsperanzaVida"].ToString();
+                            txtTipoPelo.Text = rdr["TipoDePelo"].ToString();
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+                finally
+                {
+                    //Cerrar la conexio
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
